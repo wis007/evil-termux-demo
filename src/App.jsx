63cd00326da9b04ad3617ef48bfc1452433025fc
wis007 <wisdomwisdomb@gmail.com>
2024-10-01 18:49:26 +0000

    meilleur mise en forme pour eviter le scroll horizontal

--- src/App.jsx
+++ src/App.jsx
@@ -42,29 +42,29 @@ function App() {
 
   return (
     <>
-    <header className='underline p-4' id='intro'>
-    <h1 className='text-4xl'>Les commandes <span className='uppercase text-cyan-500 '>Termux</span>, dangereuses ?</h1>
+    <header className='underline p-2 sm:p-4' id='intro'>
+    <h1 className='text-2xl sm:text-4xl font-bold'>Les commandes <span className='uppercase text-cyan-500 '>Termux</span>, dangereuses ?</h1>
     </header>
-    <main className='py-4'>
+    <main className='py-4 overflow-hidden'>
       <p>
         De nos jours la liste des tutos disponibles sur Internet est énorme et en me baladant dans certains tutos, quelle ne fut pas ma surprise de constater des scripts plutôt douteux parmis lesquels certain destinés au vol de données sensibles et dont je préfère faire taire le nom.<br />Alors j'ai decidé de mettre en place un petit site qui héberge quelques code de démonstration.<br />Bien sur chacun de ces codes sont conçus à destination de professionnels qui s'y connaissent et sauront comment s'en servir pour faire des démonstrations dans des environnements virtuels, autorisés,... bref contrôler.
       </p>
       <hr className='m-4' />
       <p className="text-red-600 pb-4">❗❗❗ L'usage de ces codes est non réversible. Donc vos données serons perdues à jamais</p>
-      <fieldset className='border p-2 rounded-lg text-center'>
+      <fieldset className='border p-2 rounded-lg text-center w-full sm:w-auto'>
         <legend>Configuration</legend>
         <form className='flex flex-col pb-2 justify-around items-center uppercase text-xl sm:flex-row gap-4' onSubmit={generate}>
           <label htmlFor="choix">Choix :</label>
-          <select name="choix" id='choix' className='bg-inherit text-red-400 border p-4 rounded-lg' onChange={generate}>
+          <select name="choix" id='choix' className='bg-inherit text-red-400 border p-2 rounded-lg w-full sm:w-auto' onChange={generate}>
           <option value="" disabled selected className='text-xl'>Sélectionnez une charge :</option>
-            <option value="1" className='text-xl text-black '>Données Whatsapp</option>
-            <option value="2" className='text-xl text-black '>Toutes les images</option>
-            <option value="3" className='text-xl text-black '>Toutes les video</option>
-            <option value="4" className='text-xl text-black '>Toutes les outils termux</option>
+            <option value="1" className='sm:text-xl text-black '>Données Whatsapp</option>
+            <option value="2" className='sm:text-xl text-black '>Toutes les images</option>
+            <option value="3" className='sm:text-xl text-black '>Toutes les video</option>
+            <option value="4" className='smtext-xl text-black '>Toutes les outils termux</option>
           </select>
-          <input className='border m-2 p-2 rounded-lg hover:cursor-pointer text-cyan-400 font-bold' type="submit" value="Recuperer" />
+          <input className='border m-2 p-2 rounded-lg hover:cursor-pointer text-cyan-400 font-bold w-full sm:w-auto' type="submit" value="Recuperer" />
         </form>
-        <div className='bg-gray-200 p-1 rounded text-black font-mono sm:text-2xl' id="resultat">
+        <div className='bg-gray-200 p-1 rounded text-black font-mono sm:text-2xl max-w-full' id="resultat">
           Veillez selectionner une valeur puis cliquer sur recuperer
         </div>
       </fieldset>
